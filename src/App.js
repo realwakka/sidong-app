908cb16bc49d08de96bbf3809d5faea11b620b5a
Sidong Yang <realwakka@gmail.com>
2021-12-05 11:08:28 +0900

    link backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { API } from 'aws-amplify';
+import React, { useState } from 'react';
 
 function App() {
+    const [text, setText] = useState('');
+    
+    const getData = async () => {
+	const data = await API.get('pythonapi', '/hello')
+	setText(data)
+	console.log(data)
+    }
+
+    getData();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+        <p>
+	  {text}
+      </p>
+	  
         <p>
           This is sidong-app!
       </p>
