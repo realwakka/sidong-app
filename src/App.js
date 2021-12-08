4bb7f3156b99434610373a75d4df0a4aec33fc3e
Sidong Yang <realwakka@gmail.com>
2021-12-09 01:55:03 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -4,29 +4,38 @@ import { API } from 'aws-amplify';
 import React, { useState } from 'react';
 
 function App() {
-    const [text, setText] = useState('');
-    
-    const getData = async () => {
-	const data = await API.get('post', '/post')
-	setText(data)
-	console.log(data)
-    }
+  const [text, setText] = useState('');
 
-    getData();
+  const addPost = async (text) => {
+    const params = {
+      headers: {},
+      response: true,
+      queryStringParameters: {
+	name: 'unknown',
+	content: text,
+      },
+    };
+    const result = await API.post('post', '/post', params);
+    console.log(result);
+  }
+
+  const getPost = async () => {
+    const result = await API.get('post', '/post');
+    setText(result.message);
+  }
+
+  // addPost('sample');
+  getPost();
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        <p>
-	  {text}
-      </p>
-	  
-        <p>
-          This is sidong-app!
-      </p>
-	  <input type="text" />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      {text}
+    </p>
+      <input type="text" />
       </header>
-    </div>
+      </div>
   );
 }
 
