f6519dff118e3bac441ecfb6815275645d164aba
Sidong Yang <sidong.yang@navercorp.com>
2021-12-10 01:17:50 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -4,71 +4,68 @@ import { API } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 
 function AddPost() {
-  const [text, setText] = useState('');
+    const [text, setText] = useState('');
 
-  const addPost = async (content) => {
-    console.log('posting ' + content);
-    const result = await API.post('post', '/post', {
-      body: {
-	name: 'unknown',
-	content: content
-      }
-    });
-    console.log(result);
-    window.location.reload();    
-  }
-  
-  const onClickSubmit = (e) => {
-    console.log('onClick!');
-    addPost(text);
-    setText('');
+    const addPost = async (content) => {
+	console.log('posting ' + content);
+	const result = await API.post('post', '/post', {
+	    body: {
+		name: 'unknown',
+		content: content
+	    }
+	});
+	console.log(result);
+	window.location.reload();    
+    }
+    
+    const onClickSubmit = (e) => {
+	console.log('onClick!');
+	addPost(text);
+	setText('');
 
-  };
+    };
 
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
-  
-  return (
-      <div className="AddPost">
-      <input value={text} type="text" onChange={onChange}/>
-      <button onClick={onClickSubmit}>
-      submit!
-      </button>
-      </div>
-  );
+    const onChange = (e) => {
+	setText(e.target.value);
+    };
+    
+    return (
+	    <div className="AddPost">
+	    <input value={text} type="text" onChange={onChange}/>
+	    <button onClick={onClickSubmit}>
+	    submit!
+	</button>
+	    </div>
+    );
 }
 
 function Post(props) {
-  return (
-      <p> {props.name} : {props.content} </p>
-  );
+    return (
+	    <p> {props.name} : {props.content} </p>
+    );
 }
 
 
 function App() {
-  const [postList, setPostList] = useState([]);
+    const [postList, setPostList] = useState([]);
 
-  useEffect(() => {
-    const getPosts = async () => {
-      const res = await API.get('post', '/post');
-      console.log(res);
-      const list = res.data.Items.map(
-	(post) => <Post key={post.id.S} name={post.name.S} content={post.content.S} />);
-      setPostList(list);
-    };
-    getPosts();
-  }, []);
-  
-  return (
-      <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {postList}
-    <AddPost/>
-      </header>
-      </div>
-  );
+    useEffect(() => {
+	const getPosts = async () => {
+	    const res = await API.get('post', '/post');
+	    console.log(res);
+	    const list = res.data.Items.map(
+		(post) => <Post key={post.id.S} name={post.name.S} content={post.content.S} />);
+	    setPostList(list);
+	};
+	getPosts();
+    }, []);
+    
+    return (
+	    <div className="App">
+	    <AddPost/>	  
+	    {postList}
+	    </div>
+    );
 }
 
 export default App;
